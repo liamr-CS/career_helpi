59ecb3a0e3706c3d5784a65d0dced087252b76da
Liam Roth <meaghanr@udel.edu>
2024-04-04 12:01:09 -0400

    Done Detailed Questions

---
+++ src/DetailedQuestions.tsx
@@ -0,0 +1,27 @@
+function Questions () {
+    return (
+        <div>
+            <p>
+                On a scale of one to five, how much do you like programming?
+            </p>
+            <p>
+                On a scale of strongly agree to strongly disagree, are you a leader?
+            </p>
+            <p>
+                Respond with how correct the following statement is: reality is objective.
+            </p>
+            <p>
+                Respond with how often you do the following: think about the future.
+            </p>
+            <p>
+                Can you see yourself in this career: a fast-paced work environment?
+            </p>
+            <p>
+                Does the following sound like you: I want a good-work life balance?
+            </p>
+            <p>
+                Which one of the following careers sounds the most appealing?
+            </p>
+        </div>
+    );
+}
