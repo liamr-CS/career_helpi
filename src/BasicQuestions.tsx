08020ca95c09e07f4329acbae070d4561a795159
Alexander <clawsonx@udel.edu>
2024-04-08 22:49:29 -0400

    created 7 basic questions

---
+++ src/BasicQuestions.tsx
@@ -0,0 +1,13 @@
+function Questions () {
+    return(
+        <div>
+            <p>Do you enjoy working with your hands?</p>
+            <p>Are you interested in technology?</p>
+            <p>Do you like solving puzzles or problems?</p>
+            <p>Do you prefer working alone or in a team?</p>
+            <p>Do you enjoy being creative?</p>
+            <p>Are you good at analyzing data or information?</p>
+            <p>Do you have clear goals for your future career?</p>
+        </div>
+    );
+}
