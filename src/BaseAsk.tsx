98f26e44798eb19d067011b0e068353f39e595a0
Alexander <clawsonx@udel.edu>
2024-04-09 11:50:04 -0400

    working on buttons

--- src/BaseAsk.tsx
+++ src/BaseAsk.tsx
@@ -4,10 +4,10 @@ function BaseQsDes() {
     return (
         <div className="basic-questions-description">
             <p>
-                -The basic career assessment consists of a mix of yes-and-no and multiple choice questions that you will answer.
+                -Answer these questions as accurately as possible while ensuring not to skip any.
             </p>
             <p>
-                -Answer these questions as accurately as possible while ensuring not to skip any.
+                -The basic career assessment consists of a mix of yes-and-no and multiple choice questions that you will answer.
             </p>
             <p>
                 -The answers provided will be used to calculate a suitable work environment for the tester and provide a list of jobs that do well in that environment.
