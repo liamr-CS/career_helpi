5d7fdb5a3ac99eb26def4a9a7cc05f0dff46c33a
Liam Roth <meaghanr@udel.edu>
2024-04-09 11:57:59 -0400

    worked on question input

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -1,7 +1,30 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
 function Questions () {
+    const [answer1, setAnswer1] = useState<string>("Yes");
+    function updateAnswer1(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer1(event.target.value);
+    }
     return(
         <div>
             <p>Do you enjoy working with your hands?</p>
+            <div>
+                <Form.Check
+                    type="radio"
+                    onChange={updateAnswer1}
+                    label="Yes"
+                    value="yes"
+                    checked={answer1 === "Yes"}
+                />
+                <Form.Check
+                    type="radio"
+                    onChange={updateAnswer1}
+                    label="No"
+                    value="no"
+                    checked={answer1 === "no"}
+                />
+            </div>
             <p>Are you interested in technology?</p>
             <p>Do you like solving puzzles or problems?</p>
             <p>Do you prefer working alone or in a team?</p>
@@ -11,3 +34,5 @@ function Questions () {
         </div>
     );
 }
+
+export default Questions
